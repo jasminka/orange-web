6d86b499cffeac9a879344e20748fe36baef043c
Jasmina <jasmina@Jasminas-MacBook-Air.local>
2017-07-25 15:07:11 +0200

    Website for Houston course.

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^text-analysis/$', TemplateView.as_view(template_name='courses/text-analysis.html')),
+    url(r'^introduction-to-data-mining/$', TemplateView.as_view(template_name='courses/introduction-to-data-mining.html')),
 ]
